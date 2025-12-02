b7c2fb1e352b3d2cacdd509283573ec3d542a427
Asad Tariq <asaddc123@outlook.com>
2025-12-02 20:58:11 +0000

    Added additional branch logic to the ALU

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -5,9 +5,11 @@ module ALU #(
     input  logic [DATA_WIDTH-1:0]   srcA_i,
     input  logic [DATA_WIDTH-1:0]   srcB_i,
     input  logic [3:0]              ALUCtrl_i,
+    input logic  [2:0]              branch_i,
 
     output logic [DATA_WIDTH-1:0]   ALUResult_o,
-    output logic Zero_o  
+    output logic                    branchTaken_o,        
+    output logic                    Zero_o  
 );
 
     always_comb begin
@@ -45,6 +47,32 @@ module ALU #(
             default: ALUResult_o = 32'd0;
         endcase
         Zero_o = (ALUResult_o == 0) ? 1'b1 : 1'b0; 
+
+        case(branch_i)
+
+
+            //BEQ 
+            3'b000: branchTaken_o = (srcA_i == srcB_i);
+        
+            //BNE
+            3'b001: branchTaken_o = (srcA_i != srcB_i);
+            
+            //BLT
+            3'b100: branchTaken_o = ($signed(srcA_i) < $signed(srcB_i));
+            
+            //BGE
+            3'b101: branchTaken_o = ($signed(srcA_i) >= $signed(srcB_i));
+            
+            //BLTU- unsigned
+            3'b110: branchTaken_o = (srcA_i < srcB_i);
+            
+            //BGEU- unsigned
+            3'b111: branchTaken_o = (srcA_i >= srcB_i);
+            
+            default: branchTaken_o = 1'b0;
+
+
+        endcase
     end
 
 endmodule
