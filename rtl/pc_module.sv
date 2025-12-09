72defa29db4ef39804f5825e72658dafcd04c75a
Asad Tariq <asaddc123@outlook.com>
2025-12-09 23:55:14 +0000

    Fix Missing Ports and Errors

--- rtl/pc_module.sv
+++ rtl/pc_module.sv
@@ -4,22 +4,17 @@ module pc_module #(
     input  logic clk,
     input  logic rst,
     input  logic PCsrc,
-    input logic [DATA_WIDTH-1:0] PCTargetE_i, //this is the jump PC value coming after Execute
-    //input  logic [DATA_WIDTH-1:0] ImmExt_i,
+    input logic [DATA_WIDTH-1:0] PCTargetE_i, 
 
     output logic [DATA_WIDTH-1:0] PC,
-    output logic [DATA_WIDTH-1:0] PC_Plus4 //this ouptut goes all the way to WriteBack
+    output logic [DATA_WIDTH-1:0] PC_Plus4 
 );
-
-    //logic [DATA_WIDTH-1:0] branch_PC;
     
     logic [DATA_WIDTH-1:0] next_PC;
     logic [DATA_WIDTH-1:0] inc_PC;
 
     assign inc_PC = PC + 32'd4;
-    
     assign PC_Plus4 = inc_PC;
-    //assign branch_PC = PC + ImmExt_i;
 
     mux #(
         .DATA_WIDTH(DATA_WIDTH)
@@ -30,17 +25,10 @@ module pc_module #(
         .out(next_PC)
     );
 
-    
-
-    // Program Counter Register
-    always_ff @(posedge clk or posedge rst) begin
+    always_ff @(posedge clk) begin
         if (rst)
-            PC <= 32'hBFC00000; // reset address
+            PC <= 32'hBFC00000;
         else
             PC <= next_PC;
     end
-    
-    
 endmodule
-
-
